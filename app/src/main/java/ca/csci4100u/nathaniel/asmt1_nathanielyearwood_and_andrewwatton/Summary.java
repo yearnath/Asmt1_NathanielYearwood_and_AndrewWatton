d8ec746061c83d7d05204bb3a018d8762a506523
yearnath <nathaniel.yearwood@uoit.net>
2017-10-25 15:10:20 -0400

    Fixed constraints, added summary page

---
+++ app/src/main/java/ca/csci4100u/nathaniel/asmt1_nathanielyearwood_and_andrewwatton/Summary.java
@@ -0,0 +1,13 @@
+package ca.csci4100u.nathaniel.asmt1_nathanielyearwood_and_andrewwatton;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Summary extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_summary);
+    }
+}
