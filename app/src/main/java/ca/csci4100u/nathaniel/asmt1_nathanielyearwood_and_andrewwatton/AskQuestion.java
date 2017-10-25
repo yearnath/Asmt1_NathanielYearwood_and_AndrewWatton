2719395b717d64cd33554c9d64840adc013ae94d
yearnath <nathaniel.yearwood@uoit.net>
2017-10-25 16:16:53 -0400

    Started with intents in MainMenu and AskQuestion

--- app/src/main/java/ca/csci4100u/nathaniel/asmt1_nathanielyearwood_and_andrewwatton/AskQuestion.java
+++ app/src/main/java/ca/csci4100u/nathaniel/asmt1_nathanielyearwood_and_andrewwatton/AskQuestion.java
@@ -1,13 +1,27 @@
 package ca.csci4100u.nathaniel.asmt1_nathanielyearwood_and_andrewwatton;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class AskQuestion extends AppCompatActivity {
 
+    String response = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ask_question);
     }
+
+    public void SelectResponse(View v) {
+        response = "button clicked"; //button.getText();        // ummmm idk wat I'm doing, make this get the text from whichever button was clicked???
+
+        Intent resultIntent = new Intent(Intent.ACTION_PICK);
+        resultIntent.putExtra("response", response);
+        setResult(RESULT_OK, resultIntent);
+        finish();
+    }
+
 }
--- app/src/main/java/ca/csci4100u/nathaniel/asmt1_nathanielyearwood_and_andrewwatton/MainMenu.java
+++ app/src/main/java/ca/csci4100u/nathaniel/asmt1_nathanielyearwood_and_andrewwatton/MainMenu.java
@@ -1,13 +1,24 @@
 package ca.csci4100u.nathaniel.asmt1_nathanielyearwood_and_andrewwatton;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainMenu extends AppCompatActivity {
+
+    public static int GET_RESPONSE_REQUEST = 4100001;
+
+    String[] responses = new String[5];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+    }
+
+    public void getResponse(View v) {
+        Intent newAskIntent = new Intent(MainMenu.this, AskQuestion.class);
+        startActivityForResult(newAskIntent, GET_RESPONSE_REQUEST);
     }
 }
