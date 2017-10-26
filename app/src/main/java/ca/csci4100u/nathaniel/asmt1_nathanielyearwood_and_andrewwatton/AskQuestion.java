1123ba6cca55e940771ef1bea5a018668d4fffd8
yearnath <nathaniel.yearwood@uoit.net>
2017-10-26 12:04:08 -0400

    Fixed GET_RESPONSE_REQUEST because it made the app crash every time you clicked start. Fixed vertical constraints in AskQuestion yes/no buttons, created arrays.xml to contain questions array, made questions textbox read in questions from array.

--- app/src/main/java/ca/csci4100u/nathaniel/asmt1_nathanielyearwood_and_andrewwatton/AskQuestion.java
+++ app/src/main/java/ca/csci4100u/nathaniel/asmt1_nathanielyearwood_and_andrewwatton/AskQuestion.java
@@ -5,21 +5,31 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class AskQuestion extends AppCompatActivity {
 
-    String response = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ask_question);
+
+        String[] questions = getResources().getStringArray(R.array.questions);
+
+        TextView lblGreeting = (TextView)findViewById(R.id.lbl_question);
+        lblGreeting.setText(questions[MainMenu.currentQuestion]);
     }
 
-    public void SelectResponse(View v) {
-        response = "button clicked"; //button.getText();        // ummmm idk wat I'm doing, make this get the text from whichever button was clicked???
-
+    public void onClickYes(View v) {
         Intent resultIntent = new Intent(Intent.ACTION_PICK);
-        resultIntent.putExtra("response", response);
+        resultIntent.putExtra("response", 1);
+        setResult(RESULT_OK, resultIntent);
+        finish();
+    }
+
+    public void onClickNo(View v) {
+        Intent resultIntent = new Intent(Intent.ACTION_PICK);
+        resultIntent.putExtra("response", 0);
         setResult(RESULT_OK, resultIntent);
         finish();
     }
--- app/src/main/java/ca/csci4100u/nathaniel/asmt1_nathanielyearwood_and_andrewwatton/MainMenu.java
+++ app/src/main/java/ca/csci4100u/nathaniel/asmt1_nathanielyearwood_and_andrewwatton/MainMenu.java
@@ -7,9 +7,10 @@ import android.view.View;
 
 public class MainMenu extends AppCompatActivity {
 
-    public static int GET_RESPONSE_REQUEST = 4100001;
+    public static int GET_RESPONSE_REQUEST = 4110;
 
-    String[] responses = new String[5];
+    private int[] responses = new int[5];
+    public static int currentQuestion = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +18,18 @@ public class MainMenu extends AppCompatActivity {
         setContentView(R.layout.activity_main_menu);
     }
 
-    public void getResponse(View v) {
+    public void ask(View v) {
         Intent newAskIntent = new Intent(MainMenu.this, AskQuestion.class);
         startActivityForResult(newAskIntent, GET_RESPONSE_REQUEST);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int responseCode, Intent resultIntent) {
+        super.onActivityResult(requestCode, responseCode, resultIntent);
+
+        if (responseCode == RESULT_OK) {
+            responses[currentQuestion] = resultIntent.getIntExtra("response", -1);
+            currentQuestion++;
+        }
     }
 }
